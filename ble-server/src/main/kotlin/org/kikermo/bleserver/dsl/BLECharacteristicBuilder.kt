ad218082e42864c0052b0c09b9218546138cc038
Enrique Ramirez <kikermo@gmail.com>
2024-09-14 22:42:28 +0100

    Add a way of setting the value for characteristic

--- ble-server/src/main/kotlin/org/kikermo/bleserver/dsl/BLECharacteristicBuilder.kt
+++ ble-server/src/main/kotlin/org/kikermo/bleserver/dsl/BLECharacteristicBuilder.kt
@@ -4,14 +4,23 @@ import org.kikermo.bleserver.BLECharacteristic
 import org.kikermo.bleserver.BLECharacteristic.AccessType
 import org.kikermo.bleserver.exception.BLEBuilderException
 import java.util.UUID
+import kotlin.concurrent.thread
 
 class BLECharacteristicBuilder {
+    private var valueChangingAction: (((ByteArray) -> Unit) -> Unit)? = null
+
     var uuid: UUID? = null
     var name: String? = null
+    var initialValue: ByteArray = byteArrayOf(0)
 
     var readAccess: AccessType.Read? = null
     var writeAccess: AccessType.Write? = null
     var notifyAccess: AccessType.Notify? = null
+
+
+    fun valueChangingAction(action: (valueSetter: (ByteArray) -> Unit) -> Unit) {
+        valueChangingAction = action
+    }
 
     fun build() = BLECharacteristic(
         uuid = uuid ?: throw BLEBuilderException(childComponent = "uuid", component = "BLECharacteristic"),
@@ -19,5 +28,14 @@ class BLECharacteristicBuilder {
         readAccess = readAccess,
         writeAccess = writeAccess,
         notifyAccess = notifyAccess
-    )
+    ).apply {
+        this.value = initialValue
+        if (readAccess != null || notifyAccess != null) {
+            thread {
+                valueChangingAction?.invoke {
+                    value = it
+                }
+            }
+        }
+    }
 }
