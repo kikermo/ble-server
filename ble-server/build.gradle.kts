36b0ae23ef20e515460bee42a1c447d187cc74c9
Enrique Ramirez <kikermo@gmail.com>
2024-09-17 22:53:08 +0100

    Fix maven-publish

--- ble-server/build.gradle.kts
+++ ble-server/build.gradle.kts
@@ -25,23 +25,18 @@ java {
 }
 
 object PubilsInfo {
-    const val groupId = "org.kikermo.bleserver"
     const val artifactId = "core"
 
     const val desc = "BleServer core API"
     const val license = "Apache-2.0"
     const val githubRepo = "kikermo/ble-server"
-    const val release = "https://s01.oss.sonatype.org/service/local/"
-    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 }
 
 publishing {
     publications {
 
         create<MavenPublication>("maven") {
-            groupId = PubilsInfo.groupId
             artifactId = PubilsInfo.artifactId
-            version = (properties["version"] as String?) ?: "0.0.1-SNAPSHOT"
 
             from(components["java"])
 
